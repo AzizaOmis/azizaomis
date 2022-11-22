d791b4453da5fc15aba4616e7bf960b889e7787a
AzizaOmis <aziza_0527@mail.ru>
2022-11-22 18:36:26 +0300

    add new changes

--- js/1.5_script.js
+++ js/1.5_script.js
@@ -3,25 +3,11 @@ window.addEventListener('DOMContentLoaded', () => {
   const mediaQuery = '(max-width: 767.9px)';
   const mediaQueryList = window.matchMedia(mediaQuery);
   let swiper = undefined;
-
-  if (mediaQueryList.matches) {
-    swiper = new Swiper(slider, {
-      slidesPerView: 1.2,
-      spaceBetween: 16,
-      pagination: {
-        el: '.swiper-pagination',
-        type: 'bullets',
-
-        clickable: true,
-      },
-    });
-  } else {
-    swiper.destroy();
-  }
   const buttonMore = document.querySelector('.cards__button-more');
   const brandsCards = document.querySelector('.brands__cards');
   const buttonIcon = buttonMore.querySelector('.more__icon');
   const buttonText = buttonMore.querySelector('.more__text');
+
   buttonMore.addEventListener('click', function () {
     if (buttonMore.classList.contains('cards__button-more--open')) {
       buttonText.textContent = 'Скрыть';
@@ -39,4 +25,19 @@ window.addEventListener('DOMContentLoaded', () => {
       brandsCards.classList.toggle('brands__cards--close');
     }
   });
+  if (mediaQueryList.matches) {
+    swiper = new Swiper(slider, {
+      slidesPerView: 1.2,
+      spaceBetween: 16,
+      pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+
+        clickable: true,
+      },
+    });
+  } else {
+    swiper.destroy();
+    swiper = undefined;
+  }
 });
